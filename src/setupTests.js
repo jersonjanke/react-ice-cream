f5d4b29565a239f73e020d48df72a29c4624f5e4
jerson janke <jersonjanke@gmail.com>
2021-03-08 11:43:55 -0300

    start project

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,18 +1,5 @@
-import 'jest-dom/extend-expect';
-
-//This is required to silence a React DOM error that will be fixed in
-//version 16.9.0. Once this is released, remove this code.
-//Related GitHub issue https://github.com/testing-library/react-testing-library/issues/281#issuecomment-480349256
-const originalError = console.error;
-beforeAll(() => {
-  console.error = (...args) => {
-    if (/Warning.*not wrapped in act/.test(args[0])) {
-      return;
-    }
-    originalError.call(console, ...args);
-  };
-});
-
-afterAll(() => {
-  console.error = originalError;
-});
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import '@testing-library/jest-dom';
