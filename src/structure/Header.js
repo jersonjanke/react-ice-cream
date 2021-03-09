9bd7d3ab09f67cfbe1b268e7735f461c658d6b6f
Jerson Janke <jersonjanke@gmail.com>
2021-03-09 20:13:55 +0000

    Creating the main navigation

--- src/structure/Header.js
+++ src/structure/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import iceCreamImg from '../assets/img/ultimate-ice-cream.svg'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
     return (
         <header>
             <img src={iceCreamImg} alt='' />
             <h1>Ultimate Ice Cream</h1>
+            <nav>
+                <NavLink to="/" activeClassName="active" axact >
+                    Menu
+                </NavLink>
+            </nav>
         </header>
     )
 }
